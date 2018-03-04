b8d2b9f08488bdd1a30319a1c0f03cbe92311633
inoabrian <brianmust1@gmail.com>
2018-03-04 02:59:44 -0500

    disabled first post and added logic to check if there are any active ones.

--- posts.js
+++ posts.js
@@ -19,24 +19,33 @@ function renderPosts(postList) {
 
     let component = document.querySelector('main > section > .container > .body > .body-posts > .body-posts-content :last-child');
 
-    component.innerHTML = postList.posts
-    .filter((post) => post.active)
-    .map((post) => {
-        return `
-            <div class="blog-post">
-                <div class="blog-post-title">
-                    <a href="./posts/${post.postLink}_${post.title.split(' ').join('')}.html" target="_blank">
-                        ${post.title}
-                    </a>
+    let activePosts = postList.posts
+    .filter((post) => post.active);
+
+    if(activePosts.length > 0) {
+        component.innerHTML = activePosts 
+        .map((post) => {
+            return `
+                <div class="blog-post">
+                    <div class="blog-post-title">
+                        <a href="./posts/${post.postLink}_${post.title.split(' ').join('')}.html" target="_blank">
+                            ${post.title}
+                        </a>
+                    </div>
+                    <div class="blog-post-description">
+                        ${post.description}
+                    </div>
+                    <div class="blog-post-timestamp">
+                        ${post.created}
+                    </div>
                 </div>
-                <div class="blog-post-description">
-                    ${post.description}
-                </div>
-                <div class="blog-post-timestamp">
-                    ${post.created}
-                </div>
-            </div>
-        `;
-    })
-    .join('<br>');
+            `;
+        })
+        .join('<br>');
+    }
+    else {
+        component.innerHTML = `<h3>
+            Posts coming soon.
+        </h3>`
+    }
 }
