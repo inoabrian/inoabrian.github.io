e5c124dcc23228989304637ab226471a89e8b1c7
inoabrian <brianmust1@gmail.com>
2018-03-04 02:27:24 -0500

    implemented render method in postjs file

--- posts.js
+++ posts.js
@@ -2,7 +2,7 @@
     function() {
         let postsJSON = {};
 
-        fetch('posts.json')
+        fetch('https://www.inoabrian.github.io/posts.json',{"mode": 'cors'})
             .then(function(res) {
                 return res.json();
             })
@@ -16,4 +16,23 @@
 
 function renderPosts(postList) {
     console.info(postList);
+
+    let component = document.querySelector('main > section > .container > .body > .body-posts > .body-posts-content :last-child');
+
+    component.innerHTML = postList
+    .map((post) => {
+        let postTemplate = `
+            <div class="blog-post">
+                <div class="blog-post-title">
+                    ${post.title}
+                </div>
+                <div class="blog-post-description">
+                    ${post.description}
+                </div>
+                <div class="blog-post-timestamp">
+                    ${post.created}
+                </div>
+            </div>
+        `;
+    });
 }
