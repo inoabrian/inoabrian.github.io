4bfc5b36bfe3a7737c69a732ac3d58edd137c0ea
Brian Inoa <binoa@ups.com>
2019-01-02 01:59:17 -0500

    Updates

---
+++ precache-manifest.28466fad8454f2d06451000ec9ee2503.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "inoabrian.github.io/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "66b3ba858cc77ce0e4ff",
+    "url": "inoabrian.github.io/static/js/runtime~main.66b3ba85.js"
+  },
+  {
+    "revision": "4ea5d33f988df3b42f49",
+    "url": "inoabrian.github.io/static/js/main.4ea5d33f.chunk.js"
+  },
+  {
+    "revision": "f03e8ae863144a61c620",
+    "url": "inoabrian.github.io/static/js/1.f03e8ae8.chunk.js"
+  },
+  {
+    "revision": "4ea5d33f988df3b42f49",
+    "url": "inoabrian.github.io/static/css/main.c7b7eafc.chunk.css"
+  },
+  {
+    "revision": "1e97caf606891359777fad6c56f7734d",
+    "url": "inoabrian.github.io/index.html"
+  }
+];
