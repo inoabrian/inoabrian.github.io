f9541fe64dc7874ab725d146da6ffe7eb8d0d338
Brian Inoa <binoa@ups.com>
2019-01-03 00:54:16 -0500

    Updates to website

--- src/Components/Portfolio.js
+++ src/Components/Portfolio.js
@@ -30,8 +30,13 @@ class Portfolio extends Component {
 
          <div className="twelve columns collapsed">
             <h3>
-              Portfolio Work - Coming Soon.
+              Portfolio - Coming Soon
             </h3>
+            {/* <h1>Check Out Some of My Works.</h1>
+
+            <div id="portfolio-wrapper" className="bgrid-quarters s-bgrid-thirds cf">
+                {projects}
+            </div> */}
           </div>
       </div>
    </section>
